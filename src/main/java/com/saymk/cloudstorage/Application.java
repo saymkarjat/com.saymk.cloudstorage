f070bea1acfab7f8105399a63ddd680ec3e61f80
saymkarjat <70850092+saymkarjat@users.noreply.github.com>
2025-01-29 02:40:30 +0300

    added liquibase config

--- src/main/java/com/saymk/cloudstorage/Application.java
+++ src/main/java/com/saymk/cloudstorage/Application.java
@@ -1,9 +1,11 @@
 package com.saymk.cloudstorage;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
