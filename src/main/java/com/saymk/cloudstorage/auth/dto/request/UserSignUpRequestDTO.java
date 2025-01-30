5119f8f31e46f5c4f70caa7378045fb5773abcb8
saymkarjat <70850092+saymkarjat@users.noreply.github.com>
2025-01-30 17:16:20 +0300

    added entity User, dto UserSignUpRequestDTO and UserLoginDTO

---
+++ src/main/java/com/saymk/cloudstorage/auth/dto/request/UserSignUpRequestDTO.java
@@ -0,0 +1,20 @@
+package com.saymk.cloudstorage.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+@Builder
+public record UserSignUpRequestDTO(
+        @NotNull(message = "Поле не может быть null.")
+        @NotBlank(message = "Поле не может быть пустым или содержать только пробелы.")
+        String username,
+        @NotNull(message = "Поле не может быть null.")
+        @NotBlank(message = "Поле не может быть пустым или содержать только пробелы.")
+        @Size(min = 8, max = 25, message = "Пароль должен иметь от 8 до 25 символов")
+        String password,
+        @NotNull(message = "Поле не может быть null.")
+        @NotBlank(message = "Поле не может быть пустым или содержать только пробелы.")
+        String confirmPassword) {
+}
---
+++ src/main/java/com/saymk/cloudstorage/auth/model/User.java
@@ -0,0 +1,21 @@
+package com.saymk.cloudstorage.auth.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+}
